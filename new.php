caecb48102cfedd6f818fc4d2722a46244de377c
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-03-08 19:22:43 -0500

    adding rest of files

---
+++ new.php
@@ -0,0 +1,19 @@
+<?php
+
+//write a script or program to brute force a wordpress site.
+//end goal - brute force
+//see how far you can get without looking up the answer. 
+
+//going to need a bunch of things to do this.
+//would take John an hour
+//make program dynamic enough to read in from a txt file
+//google most common passwords
+//massive text files that you can download.
+//how to make a program extensable
+//going for admin accounts
+// as soon as it finds the right one
+//make own site locally with wordpress.
+//set it to something you know, and include it in the dictionary so you know that it works. 
+//invetigate how to run this without 
+//curl and then look for the element that indicates a successful question
+//look up things about curl. 
